2bf730a0eb6742eb50136c1f5abe01ad43dc13c0
Pushpendra Prajapat <110169382+Pushpendra321Prajapat@users.noreply.github.com>
2022-07-28 23:49:58 +0530

    Delete java directory

--- java/HelloJava.java
+++
@@ -1,9 +0,0 @@
-public class HelloJava {
-    public static void main(String[] args) {
-        System.out.println("greeting java");
-        System.out.println("hello pushpendra");
-        System.out.println("hello pushpendra");
-
-    }
-
-}
