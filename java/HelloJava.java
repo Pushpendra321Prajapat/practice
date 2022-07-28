e6aa299560450f0968ad2c388dc1af7bcebee62d
Pushpendra Prajapat <110169382+Pushpendra321Prajapat@users.noreply.github.com>
2022-07-28 22:50:36 +0530

    Add files via upload

---
+++ java/HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("greeting java");
+        System.out.println("hello pushpendra");
+        System.out.println("hello pushpendra");
+
+    }
+
+}
